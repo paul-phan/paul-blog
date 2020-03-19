d15c5bbb3c5af48a55f85fea13bddbe43b502226
Paul Phan <phanminh65@gmail.com>
2020-03-20 02:28:22 +0700

    add my image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ function Home() {
 			   className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
 			  <div className="p-4 md:p-12 text-center lg:text-left">
 				  <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
-					   style={{backgroundImage: `url('https://source.unsplash.com/MP0IUfwrn0A')`}}/>
+					   style={{backgroundImage: `url('/paul.jpeg')`}}/>
 				  <h1 className="text-3xl font-bold pt-8 lg:pt-0">Minh T. Phan</h1>
 				  <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"/>
 				  <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
